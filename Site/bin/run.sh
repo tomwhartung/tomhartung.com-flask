b50eb25cc0909dabacf4d13c0349d06413312e74
Tom W Hartung <tomwhartung@gmail.com>
2018-12-04 10:48:11 +0800

    Updated bin/run.sh to run the tomhartung app instead of hello.

--- Site/bin/run.sh
+++ Site/bin/run.sh
@@ -3,6 +3,6 @@
 # Sanity check to test that we can render the base template
 #
 export FLASK_DEBUG=1
-export FLASK_APP=hello.py
+export FLASK_APP=tomhartung.py
 
 cd .. && python3 -m flask run --port=5002
